8521960177df056bfd7a3ebf2a455c229f8aeb67
NatipongK <87403378+NatipongK@users.noreply.github.com>
2021-10-04 08:11:57 +0700

    Add files via upload

---
+++ liff.js
@@ -0,0 +1,15 @@
+function getUserProfile() {
+    liff.getProfile().then(profile => {
+      document.getElementById("pictureUrl").src = profile.pictureUrl;
+      // document.getElementById("userId").innerHTML = profile.userId;
+      // document.getElementById("displayName").innerHTML = profile.displayName;
+      document.getElementById("getDecodedIDToken").innerHTML = liff.getDecodedIDToken().email;
+    }).catch(err => console.error(err));
+  }
+  liff.init({ liffId: "1656217711-49Bo7QWy" }, () => {
+    if (liff.isLoggedIn()) {
+      getUserProfile()
+    } else {
+      liff.login();
+    }
+  }, err => console.error(err.code, error.message));

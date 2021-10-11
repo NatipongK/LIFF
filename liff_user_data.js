1f77476eaecb60b7233273561d686d8616c599ce
NatipongK <87403378+NatipongK@users.noreply.github.com>
2021-10-11 16:54:09 +0700

    Add files via upload

--- liff_user_data.js
+++ liff_user_data.js
@@ -22,7 +22,7 @@ liff.init({ liffId: "1656217711-49Bo7QWy" }, () => {
   }
 }, err => console.error(err.code, error.message));
 
-// const btnlogOut = document.getElementById("btnlogOut");
+const btnlogOut = document.getElementById("btnlogOut");
 // function InExternalBrowser(){
 //   if(liff.getOS() === "web"){
 //     btnlogOut.style.display = "block"

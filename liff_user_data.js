a8b017fb759a760c9d9a75ada679c00e8da13965
NatipongK <87403378+NatipongK@users.noreply.github.com>
2021-10-12 16:59:08 +0700

    Add files via upload

--- liff_user_data.js
+++ liff_user_data.js
@@ -25,17 +25,7 @@ liff.init({ liffId: "1656217711-49Bo7QWy" }, () => {
 }, err => console.error(err.code, error.message));
 
 const btnlogOut = document.getElementById("btnlogOut");
-// Opera 8.0+
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-
-// Safari 3.0+ "[object HTMLElementConstructor]" 
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
-
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
 
 // Edge 20+
 var isEdge = !isIE && !!window.StyleMedia;
@@ -43,31 +33,12 @@ var isEdge = !isIE && !!window.StyleMedia;
 // Chrome 1 - 79
 var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
-// Edge (based on chromium) detection
-var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
-
-// Blink engine detection
-var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 if(isChrome='true' ){
   btnlogOut.style.display = "block"
 }
-if (isOpera='true' ){
-  btnlogOut.style.display = "block"
-}
-if(isFirefox='true' ){
-  btnlogOut.style.display = "block"
-}
-if(isSafari='true' ){
-  btnlogOut.style.display = "block"
-}
-if(isIE='true' ){
-  btnlogOut.style.display = "block"
-}
-if(sEdgeChromium='true' ){
-  btnlogOut.style.display = "block"
-}
-if(isBlink='true' ){
+
+if(isEdge='true' ){
   btnlogOut.style.display = "block"
 }
 

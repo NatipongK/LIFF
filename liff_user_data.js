7f50b5556eb3d0b2235d82cc872d84622144fb52
NatipongK <87403378+NatipongK@users.noreply.github.com>
2021-10-15 13:52:18 +0700

    Add files via upload

--- liff_user_data.js
+++ liff_user_data.js
@@ -15,9 +15,8 @@ function getUserProfileforIndex() {
 
 function displayIsInClientInfo() {
   if (liff.isInClient()) {
-    document.getElementById("btnlogOut").style.display = "block";
+    document.getElementById("button-logOut").innerHTML += '<button id="btnlogOut" onclick="logOut()">Log Out</button>';
   } 
-  document.getElementById("btnlogOut").style.display = "none";
 }
 
 
